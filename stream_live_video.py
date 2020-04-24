45a8578e6825197d97fd7298af4a33b5f623b9b1
dataplayer12 <jaiyamsharma@gmail.com>
2020-04-24 10:17:08 +0900

    improved script

--- stream_live_video.py
+++ stream_live_video.py
@@ -2,21 +2,11 @@ import cv2
 import serial
 import numpy as np
 import time
+import sys
 
-ser=serial.Serial('/dev/cu.HC-05-DevB-1',9600)
+ser=serial.Serial('/dev/cu.HC-05-DevB-2',9600)
 
-src=cv2.VideoCapture(0)#cv2.VideoCapture('welcome.MOV')
-
-ret,frame=src.read()
-count=1
-#nframes=src.get(cv2.CAP_PROP_FRAME_COUNT)
-while count<10:
-	ret,frame=src.read()
-	count+=ret
-
-cv2.namedWindow('Data sent',cv2.WINDOW_NORMAL)
-
-def send_frame(frame):
+def send_frame(ser,frame):
 	gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 	x,y=gray.shape
 
@@ -38,13 +28,67 @@ def send_frame(frame):
 			cv2.destroyAllWindows()
 			break
 
-try:
-	while ret:
-		newframe=cv2.resize(frame,(640,360))
-		send_frame(newframe)
-		print('Sent frame {}'.format(count))
+def stream_live_video(ser):
+	src=cv2.VideoCapture(0)#cv2.VideoCapture('welcome.MOV')
+	ret,frame=src.read()
+	count=1
+	#nframes=src.get(cv2.CAP_PROP_FRAME_COUNT)
+	while count<10:
 		ret,frame=src.read()
 		count+=ret
-finally:
-	pass
-	ser.close()
+
+	cv2.namedWindow('Data sent',cv2.WINDOW_NORMAL)
+
+	try:
+		while ret:
+			newframe=cv2.resize(frame,(640,360))
+			send_frame(ser,newframe)
+			print('Sent frame {}'.format(count))
+			ret,frame=src.read()
+			count+=ret
+	finally:
+		pass
+		cv2.destroyAllWindows()
+		ser.close()
+
+def send_string(ser,data,chunksize=128):
+	n=0
+	front=0
+	for x in range(0,len(data),chunksize):
+		for i in range(chunksize):
+			ser.write(data[n*chunksize+i])
+		time.sleep(0.25)
+		front=(n+1)*chunksize
+		if ser.out_waiting>250: #this depends on bluetooth module
+			ser.reset_output_buffer()
+
+	for i in range(len(data)-front):
+		ser.write(data[front+i])
+
+	print('Done')
+
+
+def main():
+	nargs=len(sys.argv)
+	try:
+		if nargs > 1:
+			ext=sys.argv[1][sys.argv[1].rfind('.'):]
+			if ext in ['jpg','jpeg','png']:
+				frame=cv2.imread(sys.argv[1],0)
+				send_frame(ser,frame)
+			else:
+				try:
+					with open(sys.argv[1],'rb') as f:
+						dataread=str(f.read())
+					send_string(ser,dataread)
+				except OSError as e:
+					print('File not found: {}'.format(str(e)))
+				except Exception as e:
+					print('An unknown error occured: {}'.format(str(e)))
+		else:
+			stream_live_video(ser)
+	finally:
+		ser.close()
+
+if __name__ == '__main__':
+	main()
